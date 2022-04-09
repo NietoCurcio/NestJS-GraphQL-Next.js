71bfab9e3bd111e3c256dd1f277f2a91743634d4
Felipe Curcio <felipe_nieto010@hotmail.com>
2022-04-08 23:14:21 -0300

    Auth0 setup

---
+++ src/pages/app/index.tsx
@@ -0,0 +1,36 @@
+import {
+  getAccessToken,
+  getSession,
+  useUser,
+  withPageAuthRequired,
+} from '@auth0/nextjs-auth0'
+import { GetServerSideProps } from 'next'
+
+export default function Home() {
+  const user = useUser()
+
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <p>{JSON.stringify(user, null, 2)}</p>
+    </div>
+  )
+}
+
+// export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+//   const session = getSession(req, res)
+
+//   if (!session) {
+//     return {
+//       redirect: {
+//         destination: '/api/auth/login',
+//         permanent: false,
+//       },
+//     }
+//   }
+
+//   return {
+//     props: {},
+//   }
+// }
+export const getServerSideProps = withPageAuthRequired()
